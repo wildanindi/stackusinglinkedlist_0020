b85f52e64ee97e3a0764f8982d9f1006d62c15cc
wildanindi <wildanindi2@gmail.com>
2024-05-11 09:55:17 +0700

    menambahkan class dan member yang berisi data

--- stackusingsinly.cpp
+++ stackusingsinly.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
 using namespace std;
+
+// Node class representing a single node in the linked list
+class Node {
+    public:
+        int data;
+        Node* next;
+
+        Node() {
+            next = NULL;
+        }
+};
