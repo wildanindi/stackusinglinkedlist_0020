97a90417fb6795cc341ce56c15fd11274704fbcb
wildanindi <wildanindi2@gmail.com>
2024-05-11 10:07:57 +0700

    menampilkan value yang di push

--- stackusingsinly.cpp
+++ stackusingsinly.cpp
@@ -29,6 +29,7 @@ class Stack {
             newNode->data = value; // 2. assign value
             newNode->next = top; // 3. set the next pointer of the new node to the current top node
             top = newNode; // 4. Update the top pointer to the new node
-            
+            cout << "Push Value: " << value << endl;
+            return value;
         }
 };
