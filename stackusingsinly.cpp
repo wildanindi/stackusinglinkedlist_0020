1312828d844149a4c948fd44ae307db25221e2a3
wildanindi <wildanindi2@gmail.com>
2024-05-11 10:36:54 +0700

    menambah perintahuntuk menghapus element top pada stack

--- stackusingsinly.cpp
+++ stackusingsinly.cpp
@@ -79,9 +79,18 @@ int main() {
 
         switch (choice){
             case 1:
-            cout << "Enter the value to push: ";
-            cin >> value;
-            stack.push(value); // Push the entered value onto the stack
+                cout << "Enter the value to push: ";
+                cin >> value;
+                stack.push(value); // Push the entered value onto the stack
+                break;
+            case 2:
+                if (!stack.isEmpty()) {
+                    stack.pop (); //pop the top element from the stack
+                }
+                else {
+                   cout << "Stack is empty. Cannot pop." << endl;
+                }
+                break;    
         }
     }
 }
