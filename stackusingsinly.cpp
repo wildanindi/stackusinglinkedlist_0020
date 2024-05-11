88fef105b9a86d0ac846ec1d44d3a02d0a5e6d22
wildanindi <wildanindi2@gmail.com>
2024-05-11 10:24:21 +0700

    mengembalikan value pada node top

--- stackusingsinly.cpp
+++ stackusingsinly.cpp
@@ -49,6 +49,11 @@ class Stack {
             }
             else{
                 Node* current = top;
-            }
+                while (current != NULL) {
+                    cout << current->data << " " << endl;
+                    current = current->next;
+                }
+                cout << endl;
+            }// return the value of the top node
         }
 };
