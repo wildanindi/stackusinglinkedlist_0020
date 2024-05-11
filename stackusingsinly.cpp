e062a6d4971ef20ef456e32962fdc7d758c274ec
wildanindi <wildanindi2@gmail.com>
2024-05-11 09:59:44 +0700

    menginisialisasi stack dengan NULL pada top pointer

--- stackusingsinly.cpp
+++ stackusingsinly.cpp
@@ -17,4 +17,9 @@ class Node {
 class Stack {
     private:
         Node* top; // pointer to the top node of the stack
+
+    public:
+        Stack(){
+            top = NULL; // initialize the stack with a null top pointer
+        }
 };
