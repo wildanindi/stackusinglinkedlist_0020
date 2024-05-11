cce6f1ee6746007122cd98e48ae2f1339beb10f6
wildanindi <wildanindi2@gmail.com>
2024-05-11 10:07:11 +0700

    update top pointer ke node baru

--- stackusingsinly.cpp
+++ stackusingsinly.cpp
@@ -28,5 +28,7 @@ class Stack {
             Node* newNode = new Node(); // 1. Allocate memory for the new node
             newNode->data = value; // 2. assign value
             newNode->next = top; // 3. set the next pointer of the new node to the current top node
+            top = newNode; // 4. Update the top pointer to the new node
+            
         }
 };
